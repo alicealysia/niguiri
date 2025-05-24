929cc691404206dc66bf93385dcf144afa4b8384
alice <alice@alysia.au>
2025-05-25 00:23:55 +1000

    app is running

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use std::path::Path;
+use gtk::prelude::*;
+use relm4::prelude::*;
+use niri_config::Config;
+
+struct App {
+    pub config: Config
+}
+
+impl SimpleComponent for App {
+    type Init = &'static str;
+    type Input = ();
+    type Output = ();
+    type Root = gtk::Window;
+    type Widgets = (gtk::Stack, gtk::StackSidebar);
+
+    fn init_root() -> Self::Root {
+        gtk::Window::builder()
+            .title("NiGuiRi")
+            .build()
+    }
+    fn init (
+        init: Self::Init,
+        root: Self::Root,
+        sender: ComponentSender<Self>
+    ) -> ComponentParts<Self> {
+        let model = Self {
+            config: Config::load(Path::new(init)).unwrap_or_default()
+        };
+
+        let stack = gtk::Stack::builder().build();
+        let sidebar = gtk::StackSidebar::builder().build();
+        sidebar.set_stack(&stack);
+
+        let widgets = (
+            stack,
+            sidebar
+        );
+
+        ComponentParts {
+            model,
+            widgets,
+        }
+    }
+}
+
+fn main() {
+    let app = RelmApp::new("au.alysia.niguiri");
+    app.run::<App>("testing/niri-test.kdl");
+}
